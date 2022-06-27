0b276ae8bb4715dec5bdf0988df910835c380f32
trucho <truc.ho@nfq.asia>
2022-06-28 06:14:09 +0700

    FE-05: Edit layout for HơtelCard

--- src/components/HotelCard/HotelCard.jsx
+++ src/components/HotelCard/HotelCard.jsx
@@ -8,7 +8,7 @@ const HotelCard = () => {
       <div className="room_card_img_wrapper">
         <img
           className="room_card_img"
-          src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+          src="https://cf.bstatic.com/xdata/images/hotel/max1280x900/328270670.jpg?k=e1d9d748699b1aa3257fb190da472def6689647b01a3c540e219fab6b771c184&o=&hp=1"
           alt=""
         />
       </div>
@@ -17,7 +17,7 @@ const HotelCard = () => {
         <div className="room_rating">Rating</div>
         <div>
           <span className="room_price">$250</span>
-          <span className="room_per_night"> /Per Night</span>
+          <span className="room_per_night"> / Night</span>
         </div>
       </div>
     </div>
