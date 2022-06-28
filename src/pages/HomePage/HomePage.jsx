7cc945526656bf60c3526498bc41407b2b80d74d
trucho <truc.ho@nfq.asia>
2022-06-28 07:41:32 +0700

    FE-05: add content for popular section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,12 @@
 // import { LargeButton } from '../../components';
-import { Col, Row } from 'antd';
+import { Col, Layout, Row, Space, Typography } from 'antd';
 import React, { useEffect, useState } from 'react';
 
 import { hotelApi } from '../../api';
 import { HotelCard } from '../../components';
+import './HomePage.scss';
 
-// import './HomePage.scss';
+const { Title } = Typography;
 
 const HomePage = () => {
   const [data, setData] = useState([]);
@@ -20,7 +21,19 @@ const HomePage = () => {
     <div className="homepage__container">
       {/* HomePage
       <LargeButton /> */}
+      <Title className="title_section" level={2}>
+        Our Most Populer Room
+      </Title>
       <Row>
+        <Col span={12} offset={6}>
+          <p className="content-section_center">
+            {/* {t('home-page.retailer.description')} */}
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page.
+          </p>
+        </Col>
+      </Row>
+      <Row gutter={8}>
         {data.map((item, index) => {
           return (
             <Col xxl={8} xl={8} lg={8} md={12} sm={12} xs={24} key={index}>
