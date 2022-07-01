596bddc7af3bee87fdcbf2e5aa4311eb302c2103
trucho <truc.ho@nfq.asia>
2022-07-02 05:37:21 +0700

    FE-13: fix vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://api.yourhometonight.com/api/login',
+    },
+    cors: true,
+  }
 })
