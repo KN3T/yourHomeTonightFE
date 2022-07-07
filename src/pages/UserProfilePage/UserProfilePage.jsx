55632b1b91fa1cef500f21affc1a21dc4368b231
quoctrang <quoctrang.le@nfq.asia>
2022-07-07 15:05:12 +0700

    hot fix homepage

--- src/pages/UserProfilePage/UserProfilePage.jsx
+++ src/pages/UserProfilePage/UserProfilePage.jsx
@@ -48,7 +48,6 @@ const UserProfilePage = () => {
   const handleSubmitForm = (values) => {};
 
   loadingContext.done();
-
   return (
     <div className="profile__container">
       <div className="profile__wrapper">
@@ -61,7 +60,10 @@ const UserProfilePage = () => {
                   size={100}
                   src="https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                 />
-                <div style={{ display: 'inline-block' }} className="border_vip">
+                <div
+                  style={{ display: 'inline-block', marginLeft: 20 }}
+                  className="border_vip"
+                >
                   <h1>{userData.fullName}</h1>
                   <span
                     className="account_email"
@@ -85,41 +87,39 @@ const UserProfilePage = () => {
             }
             key="1"
           >
-            <section className="profile__content ctn">
-              <Row gutter={[16, 0]}>
-                <Col lg={16} md={24} sm={24} xs={24}>
-                  <div className="checkout__content__form">
-                    <ProfileForm
-                      form={form}
-                      handleSubmitForm={handleSubmitForm}
-                      userData={userData}
-                    />
-                  </div>
-                </Col>
-                <Col lg={7} md={24} sm={24} xs={24} style={{ paddingTop: 15 }}>
-                  <div className="profile__content__summary">
-                    <div className="profile__content__summary__list">
-                      <div className="profile__content__summary__item">
-                        <span>{t('profile.not_trip')}</span>
-                      </div>
-                      <div className="profile__content__summary__item">
-                        <span>
-                          <a>{t('profile.find_trip')}</a>
-                        </span>
-                      </div>
-                      <div className="profile__content__summary__item">
-                        <span>
-                          <a>{t('profile.top_faq')}</a>
-                        </span>
-                      </div>
+            <Row gutter={[16, 0]}>
+              <Col lg={16} md={24} sm={24} xs={24}>
+                <div className="checkout__content__form">
+                  <ProfileForm
+                    form={form}
+                    handleSubmitForm={handleSubmitForm}
+                    userData={userData}
+                  />
+                </div>
+              </Col>
+              <Col lg={7} md={24} sm={24} xs={24} style={{ paddingTop: 15 }}>
+                <div className="profile__content__summary">
+                  <div className="profile__content__summary__list">
+                    <div className="profile__content__summary__item">
+                      <span>{t('profile.not_trip')}</span>
                     </div>
-                    <div className="profile__content__summary__total">
-                      <span>{t('profile.need_help')}</span>
+                    <div className="profile__content__summary__item">
+                      <span>
+                        <a>{t('profile.find_trip')}</a>
+                      </span>
+                    </div>
+                    <div className="profile__content__summary__item">
+                      <span>
+                        <a>{t('profile.top_faq')}</a>
+                      </span>
                     </div>
                   </div>
-                </Col>
-              </Row>
-            </section>
+                  <div className="profile__content__summary__total">
+                    <span>{t('profile.need_help')}</span>
+                  </div>
+                </div>
+              </Col>
+            </Row>
           </TabPane>
           <TabPane
             tab={
