517d56fff1bf525a548ed120df1e0ab72eb07cc6
lcnhut <nhut.le@nfq.asia>
2022-07-09 13:36:43 +0700

    test component search home

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,16 +1,52 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import SearchHome from '../Search/SearchHome/SearchHome';
 import './index.scss';
 
-const MainSection = () => {
+const MainSection = (props) => {
+  const {
+    visible,
+    date,
+    options,
+    childrenData,
+    adults,
+    cityName,
+    loading,
+    handleVisibleChange,
+    onFinish,
+    handleSearch,
+    onSelect,
+    setAdults,
+    setChildren,
+    setCityName,
+    DATE_FORMAT,
+    setDate,
+  } = props;
   const { t } = useTranslation();
 
   return (
     <div className="main__section__wrapper ctn">
       <h1>{t('main.slogan')}</h1>
-      <SearchHome />
+      <SearchHome
+        visible={visible}
+        date={date}
+        options={options}
+        childrenData={childrenData}
+        adults={adults}
+        cityName={cityName}
+        loading={loading}
+        handleVisibleChange={handleVisibleChange}
+        onFinish={onFinish}
+        handleSearch={handleSearch}
+        onSelect={onSelect}
+        setAdults={setAdults}
+        setChildren={setChildren}
+        setCityName={setCityName}
+        DATE_FORMAT={DATE_FORMAT}
+        setDate={setDate}
+      />
     </div>
   );
 };
