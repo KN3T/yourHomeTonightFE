5a2ffd993cd71963e5bd2acf4ffd62c50ce53317
lcnhut <nhut.le@nfq.asia>
2022-07-10 12:13:15 +0700

    move handle function from component to parent page

--- src/components/HotelPopulerList/HotelPopulerList.jsx
+++ src/components/HotelPopulerList/HotelPopulerList.jsx
@@ -1,66 +1,40 @@
-import { Col, Row, Spin, Typography } from 'antd';
+/* eslint-disable react/prop-types */
+import { Col, Row, Typography } from 'antd';
 import React from 'react';
-import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useLoadingContext } from 'react-router-loading';
 
 import { HotelCard } from '..';
-import { hotelApi } from '../../api';
 import './HotelPopulerList.scss';
 
 const { Title } = Typography;
 
-const HotelPopulerList = () => {
-  const loadingContext = useLoadingContext();
-  const [loading, setLoading] = useState(false);
-
+const HotelPopulerList = ({ hotelData }) => {
   const { t } = useTranslation();
-  const [data, setData] = useState([]);
-  const NumberOfHotels = 3;
-
-  const fetchHotels = async () => {
-    setLoading(true);
-    let result = await hotelApi.getAll();
-    if (result.data.status === 'success') {
-      const { data } = result;
-      setData(data.data.hotels);
-      data && loadingContext.done();
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchHotels();
-    loadingContext.done();
-  }, []);
-
   return (
-    <Spin spinning={loading}>
-      <div className="hotel_populer_list  ctn">
-        <Title className="title_section" level={2}>
-          {t('hotel.populer_hotel_title')}
-        </Title>
-        <Row className="hotel_list" gutter={[16, 0]}>
-          {data &&
-            data.slice(0, NumberOfHotels).map((item, index) => {
-              return (
-                <Col
-                  xxl={8}
-                  xl={8}
-                  lg={12}
-                  md={12}
-                  sm={24}
-                  xs={24}
-                  key={index}
-                  className="hotel_list_item_wrapper"
-                >
-                  <HotelCard {...item} className="hotel_list_item" />
-                </Col>
-              );
-            })}
-        </Row>
-      </div>
-    </Spin>
+    <div className="hotel_populer_list  ctn">
+      <Title className="title_section" level={2}>
+        {t('hotel.populer_hotel_title')}
+      </Title>
+      <Row className="hotel_list" gutter={[16, 0]}>
+        {hotelData &&
+          hotelData.slice(0, 3).map((item, index) => {
+            return (
+              <Col
+                xxl={8}
+                xl={8}
+                lg={12}
+                md={12}
+                sm={24}
+                xs={24}
+                key={index}
+                className="hotel_list_item_wrapper"
+              >
+                <HotelCard {...item} className="hotel_list_item" />
+              </Col>
+            );
+          })}
+      </Row>
+    </div>
   );
 };
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLoadingContext } from 'react-router-loading';
 
+import { hotelApi } from '../../api';
 import { CityIntro, HotelPopulerList, MainSection } from '../../components';
 import './HomePage.scss';
 
 const HomePage = () => {
+  const [hotelData, setHotelData] = useState([]);
+  const loadingContext = useLoadingContext();
+
+  const fetchHotels = async () => {
+    let result = await hotelApi.getAll();
+    if (result.data.status === 'success') {
+      const { data } = result;
+      setHotelData(data.data.hotels);
+      data && loadingContext.done();
+    }
+  };
+
+  useEffect(() => {
+    fetchHotels();
+  }, []);
+
   return (
     <div className="homepage__container">
       <MainSection />
-      <HotelPopulerList loading />
+      <HotelPopulerList hotelData={hotelData} />
       <CityIntro />
     </div>
   );
