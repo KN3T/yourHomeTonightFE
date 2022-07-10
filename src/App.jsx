81297213583641118508207bbc985e45bac9c3a2
lcnhut <nhut.le@nfq.asia>
2022-07-10 12:56:02 +0700

    change HashRouter to BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { ConfigProvider } from 'antd';
 import 'antd/dist/antd.variable.min.css';
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './App.scss';
 import Router from './router/Router';
@@ -14,11 +14,11 @@ ConfigProvider.config({
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <div className="App">
         <Router />
       </div>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
